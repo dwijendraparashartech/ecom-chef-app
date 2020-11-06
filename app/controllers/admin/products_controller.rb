6ce51e76d6db1fd77cd73a5f23e0f2d9ce93eb5e
Dwijendra Parashar <dwijendraparashartech@gmail.com>
2020-11-06 19:23:16 +0530

    Create by Admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,13 +1,30 @@
 class Admin::ProductsController < Admin::ApplicationController
-    def index
-        @products = Product.all
-    end
-    def show
-        @product = Product.find_by_id(params[:id])
-    end
-    def update
-        @product = Product.find_by_id(params[:id])
-        @product.update({status:true})
-        redirect_back(fallback_location: root_path)
-    end
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find_by_id(params[:id])
+  end
+
+  def update
+    @product = Product.find_by_id(params[:id])
+    @product.update({status:true})
+    redirect_back(fallback_location: root_path)
+  end
+
+  # GET /products/new
+  def new
+    @product = Product.new
+  end
+
+  # GET /products/1/edit
+  def edit
+  end
+
+  def destroy
+    @product = Product.find_by_id(params[:id])
+    @product.destroy
+    redirect_back(fallback_location: root_path)
+  end
 end
